05856280636fce840b13874e821d0c1a60094454
liquanhui-99 <55009116+liquanhui-99@users.noreply.github.com>
2023-06-06 22:26:18 +0800

    feat: 添加测试

--- cache/cache_redis_test.go
+++ cache/cache_redis_test.go
@@ -94,6 +94,18 @@ func TestRedisCache_Get(t *testing.T) {
 			},
 			wantValue: "value1",
 		},
+		{
+			name: "get error",
+			key:  "key2",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				status := redis.NewStringCmd(context.Background())
+				status.SetErr(ErrKeyNotExisted)
+				cmd.EXPECT().Get(context.Background(), "key2").Return(status)
+				return cmd
+			},
+			wantErr: ErrKeyNotExisted,
+		},
 	}
 
 	for _, tc := range testCases {
@@ -104,6 +116,39 @@ func TestRedisCache_Get(t *testing.T) {
 			val, err := c.Get(context.Background(), tc.key)
 			assert.Equal(t, tc.wantErr, err)
 			fmt.Println(val)
+		})
+	}
+}
+
+func TestRedisCache_Delete(t *testing.T) {
+	testCases := []struct {
+		name    string
+		key     string
+		mock    func(ctrl *gomock.Controller) redis.Cmdable
+		wantErr error
+	}{
+		{
+			name: "delete key",
+			key:  "key1",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				status := redis.NewStatusCmd(context.Background())
+				status.SetErr(nil)
+				cmd.EXPECT().Del(context.Background(), "key1").Return(status)
+				return cmd
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			c := NewRedisCache(tc.mock(ctrl))
+			cnt, err := c.Delete(context.Background(), tc.key)
+			assert.Equal(t, tc.wantErr, err)
+			fmt.Println(cnt)
 		})
 	}
 }
